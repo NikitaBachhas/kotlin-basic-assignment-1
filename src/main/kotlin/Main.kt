6d4ae813391394a16dd4f466c7e68f8be63cf04b
NikitaBachhas <nikita.bachhas@thoughtworks.com>
2024-07-31 00:09:27 +0800

    [Kotlin Assignment 1 - Practice A-ZZZ] Edited main method to take in arguments as starting sequence number and number of results

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,10 @@
 import com.thoughtworks.kotlin_basic.util.ConversionOfNumbersToColumnSequenceLabels
-//import com.thoughtworks.kotlin_basic.util.PrintUtil
 
-//fun main(args: Array<String>) {
-//    println("Hello World!")
-//    println("Program arguments: ${args.joinToString()}")
-//
-//    val printUtil = PrintUtil()
-//    // Try adding program arguments via Run/Debug configuration.
-//    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//
-//    val headers = listOf("ID", "Name", "Occupation")
-//    val rows = listOf(
-//        listOf("1", "Alice", "Software Engineer"),
-//        listOf("2", "Bob", "Data Scientist"),
-//        listOf("3", "Charlie", "Product Manager")
-//    )
-//
-//    printUtil.printTable(headers, rows)
-//}
+fun main(args: Array<String>) {
+    val startingSequenceNumber = args[0].toInt()
+    val numberOfResults = args[1].toInt()
 
-fun main() {
-    println("Hello World!")
-    val validationOfNumbers = ConversionOfNumbersToColumnSequenceLabels()
-    println( validationOfNumbers.validationOfNumbers(1,1))
+    val numbersToColumnSequenceLabels = ConversionOfNumbersToColumnSequenceLabels()
+
+    println("[" + numbersToColumnSequenceLabels.generatingColumnLabels(startingSequenceNumber, numberOfResults) + "]")
 }
